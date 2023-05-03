b10a846cef7bf0c1ea98dd9d7deddf01e723ad3a
ihor <ihor.bielchenko@gmail.com>
2023-05-03 07:38:39 +0200

    Fix tag service.

--- src/api/tag-option/tag-option.service.ts
+++ src/api/tag-option/tag-option.service.ts
@@ -13,12 +13,15 @@ import { TagOption } from './tag-option.entity';
 export class TagOptionService extends OptionService {
 	protected readonly mainRelationColumnName: string = 'tagId';
 	protected readonly optionRelationColumnName: string = 'tagOptionId';
+	protected readonly optionContentColumnName: string = 'tagOptionId';
 	protected readonly repositoryConstructor = TagOption;
 	protected readonly repositoryOptionConstructor = TagTagOption;
+	protected readonly repositoryContentOptionConstructor = TagTagOption;
 
 	constructor(
 		@InjectRepository(TagOption) protected readonly repository: Repository<TagOption>,
 		@InjectRepository(TagTagOption) public readonly repositoryOption: Repository<TagTagOption>,
+		@InjectRepository(TagTagOption) public readonly repositoryContentOption: Repository<TagTagOption>,
 		protected readonly connection: Connection,
 		protected readonly repositoryCache: CacheService,
 	) {
